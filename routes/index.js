5ac76f856369b099337817cd53f215364d5e47dc
2Gunz <mlujan476@gmail.com>
2021-07-18 14:46:11 -0600

    new

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Default Title' });
+  res.render('index', { title: 'aww yiss' });
 });
 
 module.exports = router;
