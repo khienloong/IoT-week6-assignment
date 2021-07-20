97de3b3a28d813a3ce12b162e85f8b4f7f066d57
2Gunz <mlujan476@gmail.com>
2021-07-20 16:34:39 -0600

    test db post

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,30 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res, next) =>{
+  var conn = mysql.createConnection({
+    host      : 'logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com',
+    user      : 'logo2',
+    password  : 'logologo',
+    port      : '3306'
+  });
+
+  conn.connect((err)=>{
+    if(err) throw err;
+    var sql = 'INSERT INTO page(title, body) VALUES('+req.body.title+','+req.body.body+')';
+    conn.query(sql, (err, result)=>{
+      if(err) throw err;
+      console.log("1 record inserted");
+    });
+  });
+
+  sql = 'SELECT * FROM page WHERE id = ( SELECT MAX(id) FROM page )';
+  conn.query(sql, (err, result)=>{
+    if(err) throw err;
+    var postTitle = result.title;
+    var postBody = result.body;
+  });
   
-  res.render('index', { title: req.body.title, body: req.body.body});
+  res.render('index', { title: postTitle, body: postBody});
 });
 
 module.exports = router;
