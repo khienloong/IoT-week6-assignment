0c7c14b50e729627776cb5c1fd40617777427379
2Gunz <mlujan476@gmail.com>
2021-07-20 15:26:21 -0600

    test post

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'aww yiss' });
+  res.render('index', { title: 'default title', body: 'default body' });
+});
+
+router.post('/', (req, res, next) =>{
+  res.render('index', { title: res.title, body: res.body});
 });
 
 module.exports = router;
