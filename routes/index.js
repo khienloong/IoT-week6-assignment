ccf53c9a8fccecc5193f10dde634472104c4a6d6
2Gunz <mlujan476@gmail.com>
2021-07-20 19:32:51 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,31 @@ var mysql = require("mysql");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "default title", body: "default body" });
+  var conn = mysql.createConnection({
+    host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
+    user: "logo2",
+    password: "logologo",
+    database: "mydb",
+  });
+  conn.connect((err) => {
+    if (err) throw err;
+    sql = "SELECT * FROM page";
+    var rows;
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      if (result != "") {
+        rows = JSON.parse(JSON.stringify(result[result.length - 1]));
+        console.log(rows);
+        console.log(rows["title"]);
+        /* postTitle = rows['title'];
+      postBody = rows['body']; */
+        res.render("index", { title: rows["title"], body: rows["body"] });
+      } else
+        res.render("index", { title: "default title", body: "default body" });
+    });
+  });
 });
-
 
 router.post("/", (req, res, next) => {
   var conn = mysql.createConnection({
