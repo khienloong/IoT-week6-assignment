b1b5ed2e331059bb8b7ae6e8f5c26f0ad1aed7c0
2Gunz <mlujan476@gmail.com>
2021-07-20 18:49:39 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,48 @@ var express = require("express");
 var router = express.Router();
 var mysql = require("mysql");
 
+var conn = mysql.createConnection({
+  host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
+  user: "logo2",
+  password: "logologo",
+  database: "mydb",
+});
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "default title", body: "default body" });
+
+  conn.connect((err) => {
+    if (err) throw err;
+    sql = "SELECT * FROM page";
+    var rows;
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      rows = JSON.parse(JSON.stringify(result[result.length - 1]));
+      console.log(rows);
+      console.log(rows["title"]);
+      /* postTitle = rows['title'];
+    postBody = rows['body']; */
+      res.render("index", { title: rows["title"] ? rows['title'] : "Default Title", body: rows["body"] ? rows['body'] : "Default Body" });
+    });
+  })
+
+  
 });
 
+router.get('/delete', (req,res,next)=>{
+  conn.connect((err)=>{
+    if(err) throw err;
+    var sql = "DELETE * FROM page";
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log("All records deleted");
+      res.render("index", { title: "default title", body: "default body" });
+    });
+  })
+})
+
 router.post("/", (req, res, next) => {
-  var conn = mysql.createConnection({
-    host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
-    user: "logo2",
-    password: "logologo",
-    database: "mydb",
-  });
+  
 
   conn.connect((err) => {
     if (err) throw err;
