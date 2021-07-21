48423d628ec1c4000372c8c14ff02bfbd69c4b90
2Gunz <mlujan476@gmail.com>
2021-07-20 19:09:09 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -2,74 +2,86 @@ var express = require("express");
 var router = express.Router();
 var mysql = require("mysql");
 
-var conn = mysql.createConnection({
-  host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
-  user: "logo2",
-  password: "logologo",
-  database: "mydb",
-});
-conn.connect((err) => {
-  if (err) throw err;
-});
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  sql = "SELECT * FROM page";
-  var rows;
-  conn.query(sql, (err, result) => {
+  var conn = mysql.createConnection({
+    host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
+    user: "logo2",
+    password: "logologo",
+    database: "mydb",
+  });
+
+  conn.connect((err) => {
     if (err) throw err;
-    console.log(result);
-    if(rows = JSON.parse(JSON.stringify(result[result.length - 1])))
-    res.render("index", {
-      title: rows["title"],
-      body: rows["body"]
-    });
-    else
-    res.render("index", {
-      title: "No records in Db",
-      body: ""
-    });
-
-    console.log(rows);
-    console.log(rows["title"]);
-    /* postTitle = rows['title'];
+    sql = "SELECT * FROM page";
+    var rows;
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      rows = JSON.parse(JSON.stringify(result[result.length - 1]));
+      console.log(rows);
+      console.log(rows["title"]);
+      /* postTitle = rows['title'];
     postBody = rows['body']; */
-
-    
+      res.render("index", { title: rows["title"], body: rows["body"] });
+    });
+    //res.render("index", { title: "default title", body: "default body" });
   });
 });
 
 router.get("/delete", (req, res, next) => {
-  var sql = "DELETE FROM page";
-  conn.query(sql, (err, result) => {
+  var conn = mysql.createConnection({
+    host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
+    user: "logo2",
+    password: "logologo",
+    database: "mydb",
+  });
+
+  conn.connect((err) => {
     if (err) throw err;
-    console.log("All records deleted");
-    res.render("index", { title: "default title", body: "default body" });
+    sql = "DELETE FROM page";
+    var rows;
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+
+      res.render("index", { title: "No Records in Db", body: "" });
+    });
   });
 });
 
 router.post("/", (req, res, next) => {
-  var sql =
-    'INSERT INTO page(title, body) VALUES("' +
-    req.body.title +
-    '","' +
-    req.body.body +
-    '")';
-  conn.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log("1 record inserted");
+  var conn = mysql.createConnection({
+    host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
+    user: "logo2",
+    password: "logologo",
+    database: "mydb",
   });
 
-  sql = "SELECT * FROM page";
-  var rows;
-  conn.query(sql, (err, result) => {
+  conn.connect((err) => {
     if (err) throw err;
-    console.log(result);
-    rows = JSON.parse(JSON.stringify(result[result.length - 1]));
-    console.log(rows);
-    console.log(rows["title"]);
-    /* postTitle = rows['title'];
+    var sql =
+      'INSERT INTO page(title, body) VALUES("' +
+      req.body.title +
+      '","' +
+      req.body.body +
+      '")';
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+
+    sql = "SELECT * FROM page";
+    var rows;
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      rows = JSON.parse(JSON.stringify(result[result.length - 1]));
+      console.log(rows);
+      console.log(rows["title"]);
+      /* postTitle = rows['title'];
     postBody = rows['body']; */
-    res.render("index", { title: rows["title"], body: rows["body"] });
+      res.render("index", { title: rows["title"], body: rows["body"] });
+    });
   });
 });
 
