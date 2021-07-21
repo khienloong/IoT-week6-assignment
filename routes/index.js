3e327ba6239b6261debdd19445a49f0525df9c33
2Gunz <mlujan476@gmail.com>
2021-07-21 16:41:43 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -49,62 +49,7 @@ router.get("/delete", (req, res, next) => {
   });
 })
 
-router.delete("/", (req, res, next) => {
-  var conn = mysql.createConnection({
-    host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
-    user: "logo2",
-    password: "logologo",
-    database: "mydb",
-  });
-
-  conn.connect((err) => {
-    if (err) throw err;
-    sql = "DELETE FROM page";
-    var rows;
-    conn.query(sql, (err, result) => {
-      if (err) throw err;
-      res.render("index", { title: "Records deleted", body: "" });
-    })
-  });
-})
-
 router.post("/", (req, res, next) => {
-  var conn = mysql.createConnection({
-    host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
-    user: "logo2",
-    password: "logologo",
-    database: "mydb",
-  });
-
-  conn.connect((err) => {
-    if (err) throw err;
-    var sql =
-      'INSERT INTO page(title, body) VALUES("' +
-      req.body.title +
-      '","' +
-      req.body.body +
-      '")';
-    conn.query(sql, (err, result) => {
-      if (err) throw err;
-      console.log("1 record inserted");
-    });
-
-    sql = "SELECT * FROM page";
-    var rows;
-    conn.query(sql, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      rows = JSON.parse(JSON.stringify(result[result.length - 1]));
-      console.log(rows);
-      console.log(rows["title"]);
-      /* postTitle = rows['title'];
-    postBody = rows['body']; */
-      res.render("index", { title: rows["title"], body: rows["body"] });
-    });
-  });
-});
-
-router.put("/", (req, res, next) => {
   var conn = mysql.createConnection({
     host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
     user: "logo2",
