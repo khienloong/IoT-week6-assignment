7498274aa79f28ff176a31f2f0954f9db50c80e7
2Gunz <mlujan476@gmail.com>
2021-07-21 18:09:30 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 var mysql = require("mysql");
 
-
 router.get("/delete", (req, res, next) => {
   var conn = mysql.createConnection({
     host: "logo2.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
@@ -35,11 +34,19 @@ router.get("/", function (req, res, next) {
     var rows;
     conn.query(sql, (err, result) => {
       if (err) throw err;
- 
+
       if (result != "") {
         rows = JSON.parse(JSON.stringify(result[result.length - 1]));
 
-        res.send("\r\nThe latest entry in the DB is:\r\n" +rows["id"] + ": " + rows["title"] + " " + rows["body"]+"\r\n");
+        res.send(
+          "\r\nThe latest entry in the DB is:\r\n" +
+            rows["id"] +
+            ": " +
+            rows["title"] +
+            " " +
+            rows["body"] +
+            "\r\n"
+        );
       } else res.send("\r\nThere are no records in DB :( \r\n");
     });
   });
@@ -56,9 +63,8 @@ router.get("/:id", function (req, res, next) {
   var id = req.params.id;
   id = parseInt(id);
 
-  
-
-
+  if (id == "NaN") res.send("\r\nThe entered id value must be an integer.\r\n");
+  else {
     conn.connect((err) => {
       if (err) throw err;
       sql = "SELECT * FROM page WHERE id = '" + id + "'";
@@ -70,15 +76,15 @@ router.get("/:id", function (req, res, next) {
           rows = JSON.parse(JSON.stringify(result[result.length - 1]));
 
           /* postTitle = rows['title'];
-        postBody = rows['body']; */
-          res.send("\r\n" + id + ": " + rows["title"] + " " + rows["body"] + "\r\n");
+      postBody = rows['body']; */
+          res.send(
+            "\r\n" + id + ": " + rows["title"] + " " + rows["body"] + "\r\n"
+          );
         } else res.send("\r\n There's currently no data :(\r\n");
       });
     });
-
+  }
 });
-
-
 
 router.get("/delete/:id", (req, res, next) => {
   var conn = mysql.createConnection({
@@ -89,18 +95,20 @@ router.get("/delete/:id", (req, res, next) => {
   });
 
   var id = req.params.id;
+  id = parseInt(id);
 
-  if (!isNaN(id) && Number.isInteger(id)) {
+  if (id == "NaN") res.send("\r\nThe entered id value must be an integer.\r\n");
+  else {
     conn.connect((err) => {
       if (err) throw err;
-      sql = "DELETE FROM page WHERE id ='" + id +"'";
+      sql = "DELETE FROM page WHERE id ='" + id + "'";
       var rows;
       conn.query(sql, (err, result) => {
         if (err) res.send("\r\n There is no record with that value\r\n");
         res.send("\r\n Record " + id + " has been deleted.\r\n");
       });
     });
-  } else res.send("\r\n The record value entered must be an integer :( \r\n");
+  }
 });
 
 router.post("/", (req, res, next) => {
@@ -128,7 +136,8 @@ router.post("/", (req, res, next) => {
       "SELECT * FROM page WHERE title = '" +
       req.body.title +
       "' AND body = '" +
-      req.body.body + "'";
+      req.body.body +
+      "'";
     var rows;
     conn.query(sql, (err, result) => {
       if (err) throw err;
