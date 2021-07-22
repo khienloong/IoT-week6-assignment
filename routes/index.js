ae8009a5ab5fd6c44f0dc36e7340b1e8ad91a1bf
2Gunz <mlujan476@gmail.com>
2021-07-21 18:14:41 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get("/delete/:id", (req, res, next) => {
   var id = req.params.id;
   id = parseInt(id);
 
-  if (id == "NaN") res.send("\r\nThe entered id value must be an integer.\r\n");
+  if (isNaN(id)) res.send("\r\nThe entered id value must be an integer.\r\n");
   else {
     conn.connect((err) => {
       if (err) throw err;
@@ -61,7 +61,7 @@ router.get("/:id", function (req, res, next) {
   id = parseInt(id);
   console.log(id);
 
-  if (id == NaN) res.send("\r\nThe entered id value must be an integer.\r\n");
+  if (isNaN(id)) res.send("\r\nThe entered id value must be an integer.\r\n");
   else {
     conn.connect((err) => {
       if (err) throw err;
